99dbed50270baaced59b40f79c1bcf926179969e
kideveloper612 <developer61510@gmail.com>
2020-06-22 09:36:40 +0800

    some update

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ import os
 import csv
 import glob
 import zipfile
+import rarfile
 import pandas as pd
-
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -26,23 +26,17 @@ def db_create(db_name):
                      "city VARCHAR(30), "
                      "state VARCHAR(20), "
                      "zip VARCHAR(10), "
-                     "phone VARCHAR(15), "
+                     "phone VARCHAR(15) UNIQUE, "
                      "ip VARCHAR(20), "
+                     "gender VARCHAR(20), "
                      "DOB VARCHAR(20), "
-                     "gender VARCHAR(3), "
-                     "PRIMARY KEY(id)"
+                     "PRIMARY KEY(id) "
                      ")".format(db_name))
 
 
-def read_file(name):
-    with open(file=name, encoding='utf-8', mode='r') as csv_file:
-        rows = list(csv.reader(csv_file))
-    return rows
-
-
-def get_file_paths():
-    directory = 'F:\\working\\python\\scrapping\\Location_Marco\\Newly\\Individuals\\fastpeoplesearch\\Database\\'
-    files = []
+def main():
+    # db_create('customers')
+    directory = 'F:\\working\\python\\scrapping\\Location_Marco\\Newly\\Individuals\\fastpeoplesearch\\Database\\440_files\\'
     for name in glob.glob('{}*.zip'.format(directory)):
         base = os.path.basename(name)
         filename = os.path.splitext(base)[0]
@@ -50,23 +44,19 @@ def get_file_paths():
         archive = '.'.join([data, 'zip'])
         full_path = ''.join([directory, archive])
         csv_file = '.'.join([data, 'csv'])
-        files.append(csv_file)
-    return files, full_path
-
-
-def main():
-    full_path, file_names = get_file_paths()
-    for file_name in file_names:
         zf = zipfile.ZipFile(full_path)
         df = pd.read_csv(zf.open(csv_file))
-        print(df.values.tolist()[2])
-    db_create('customers')
-    path = '166.csv'
-    lines = read_file(name=path)
-    sql = "INSERT INTO customers (email, family, given, address, city, state, zip, phone, ip, DOB, gender) VALUES (%s, " \
-          "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    mycursor.executemany(sql, lines)
-    print(mycursor.rowcount, "was inserted.")
+        df = df.astype(object).where(pd.notnull(df), None)
+        sql = "INSERT INTO customers (email, family, given, address, city, state, zip, phone, ip, DOB, gender) " \
+              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        print(csv_file, full_path)
+        try:
+            mycursor.executemany(sql, df.values.tolist())
+            mydb.commit()
+            print(csv_file, "was inserted.")
+        except mysql.connector.errors as e:
+            print(e)
+            continue
 
 
 if __name__ == '__main__':
